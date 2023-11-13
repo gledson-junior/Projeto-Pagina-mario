4262cb9aced758c398810729163936eed63bb096
047juniorr <gledson.enterprise@gmail.com>
2023-11-12 23:27:18 -0300

    Projeto Mario

---
+++ script.js
@@ -0,0 +1,15 @@
+let EntrarContato = document.querySelector(".mario-form")
+let MascaraForm = document.querySelector(".mascara")
+let EnviarForm = document.querySelector(".enviar-form")
+
+console.log(EntrarContato)
+console.log(EnviarForm)
+
+function FaleConosco () {
+    EntrarContato.style.left = "700px"
+    MascaraForm.style.visibility = "visible"
+}
+
+    function ButtonForm (){
+        EnviarForm.style.left = "-230px"
+    }
